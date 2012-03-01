5ff6561127d01e0b4ceca028800aa6cf14b64dce
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-02-29 19:33:40 -0800

    Testing

---
+++ gpkg/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "os"
+
+func main() {
+	gvm_root := os.Getenv("GVM_ROOT")
+	if gvm_root == "" {
+		println("ERROR: gpkg requires gvm to run (http://github.com/moovweb/gvm)")
+	} else {
+		println("GVM Package Manager!")
+	}
+}
