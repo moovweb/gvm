4f0d8e3c5e88df85e5ffe8a8e1578606e2133e19
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-02-29 21:33:28 -0800

    Updated gpkg main

--- gpkg/main.go
+++ gpkg/main.go
@@ -1,12 +1,26 @@
 package main
 
 import "os"
+import "github.com/moovweb/gvm/gpkg/pkgs"
+
+func showUsage() {
+	println("usage: gpkg list")
+}
 
 func main() {
 	gvm_root := os.Getenv("GVM_ROOT")
 	if gvm_root == "" {
 		println("ERROR: gpkg requires gvm to run (http://github.com/moovweb/gvm)")
+		os.Exit(1)
+	}
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "list" {
+			pkgs.List(gvm_root)
+		} else {
+			showUsage()
+		}
 	} else {
-		println("GVM Package Manager!")
+		showUsage()
 	}
 }
