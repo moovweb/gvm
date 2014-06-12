3ecd74b96e7f36cd8cf8aa104f4ccc1dd008147f
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-06-12 15:11:10 -0700

    Older versions of go now require patches on newer systems

--- tests/gvm_alias_comment_test.sh
+++ tests/gvm_alias_comment_test.sh
@@ -6,14 +6,14 @@ gvm alias delete bar
 #######################
 
 gvm alias # status=0
-gvm alias create foo go1.2 # status=0
-gvm alias create bar go1.0.3 # status=0
-gvm alias list # status=0; match=/gvm go aliases/; match=/foo \(go1\.2\)/; match=/bar \(go1\.0\.3\)/
+gvm alias create foo go1.2.2 # status=0
+gvm alias create bar go1.1.1 # status=0
+gvm alias list # status=0; match=/gvm go aliases/; match=/foo \(go1\.2\.2\)/; match=/bar \(go1\.1\.1\)/
 gvm use foo # status=0
-go version # status=0; match=/go1\.2/
+go version # status=0; match=/go1\.2\.2/
 gvm use bar # status=0
-go version # status=0; match=/go1\.0\.3/
+go version # status=0; match=/go1\.1\.1/
 gvm alias delete foo
-gvm alias list # status=0; match=/gvm go aliases/; match!=/foo \(go1\.2\)/; match=/bar \(go1\.0\.3\)/
+gvm alias list # status=0; match=/gvm go aliases/; match!=/foo \(go1\.2\.2\)/; match=/bar \(go1\.1\.1\)/
 gvm alias delete bar
-gvm alias list # status=0; match=/gvm go aliases/; match!=/foo \(go1\.2\)/; match!=/bar \(go1\.0\.3\)/
+gvm alias list # status=0; match=/gvm go aliases/; match!=/foo \(go1\.2\.2\)/; match!=/bar \(go1\.1\.1\)/
--- tests/gvm_use_comment_test.sh
+++ tests/gvm_use_comment_test.sh
@@ -1,5 +1,5 @@
 source $GVM_ROOT/scripts/gvm
-gvm use go1.2 # status=0
-go version # status=0; match=/go1\.2/
-gvm use go1.0.3 # status=0
-go version # status=0; match=/go1\.0\.3/
+gvm use go1.2.2 # status=0
+go version # status=0; match=/go1\.2\.2/
+gvm use go1.1.1 # status=0
+go version # status=0; match=/go1\.1\.1/
