ca118afd25566d79e384373ccb02a7e1e9e43713
Chris Neale <cin210@nyu.edu>
2016-08-24 18:21:44 -0700

    Revert forcing CGO_ENABLED to 0 if not present, that is overkill

--- tests/00gvm_install_comment_test.sh
+++ tests/00gvm_install_comment_test.sh
@@ -8,7 +8,7 @@ gvm uninstall go1.2.2 > /dev/null 2>&1
 gvm install go1.4 #status=0
 GOROOT_BOOTSTRAP=$GVM_ROOT/gos/go1.4 gvm install master #status=0
 gvm list #status=0; match=/master/
-gvm install go1.1.1 #status=0
+CGO_ENABLED=0 gvm install go1.1.1 #status=0
 gvm list #status=0; match=/go1.1.1/
 gvm install go1.2.2   #status=0
 gvm list #status=0; match=/go1.2.2/
