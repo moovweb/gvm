263ac5ce8259792a3ff491c4eb26af336de07925
Vardan Pogosian <vardan.pogosyan@gmail.com>
2019-08-30 19:52:32 +0300

    get go version from .go-version

--- tests/gvm_use_comment_test.sh
+++ tests/gvm_use_comment_test.sh
@@ -3,3 +3,11 @@ gvm use go1.7.6 # status=0
 go version # status=0; match=/go1\.7\.6/
 gvm use go1.6.4 # status=0
 go version # status=0; match=/go1\.6\.4/
+
+echo "go1.7.6" > .go-version
+gvm use # status=0
+go version # status=0; match=/go1\.7\.6/
+
+echo "go1.6.4" > .go-version
+gvm use --default # status=0
+go version # status=0; match=/go1\.6\.4/
